b21e036e4aeec52825ae142765c7a5d9cfc2c111
Jasper Vimm <jasper.vimm@gmail.com>
2024-03-08 11:13:08 +0200

    Self, me ja protected

--- algus.php
+++ algus.php
@@ -1,27 +1,50 @@
 <?php
 
-// library
+class Box {
+    public $size;
+    protected $width;
+    public static $number;
 
-class Job {
-    public function task ($logger){
-        for($i = 0; $i<10; $i++){
-            $logger ->log("task done! # $i");
-        }
+    public static function getNumber(){
+        echo self::$number;
+    }
+    public function getSize(){
+        echo $this->size . self::$number;
+    }
+    public static function me1(){
+        var_dump(self::class);
+    }
+    public static function me2(){
+        var_dump(static::class);
+    }
+    public function getWidth(){
+        var_dump($this->width);
     }
 }
 
-class ConsoleLogger {
-    public function log($message){
-        echo $message . "\n";
+class MetalBox extends Box {
+    public function getWidth2(){
+        var_dump($this->width);
     }
 }
 
-// usage code
-class NothingLogger{
-    public function log ($message)
-    
-}
+$box1 = new Box();
+$box1->size = 10;
+$box1->width = 10;
+$box1->getWidth();
+$metal1 = new MetalBox;
+$metal1->getWidth2();
+Box::$number = 10;
+$box1->getSize();
+Box::getNumber();
 
+//$box2 = new Box();
+//$box2->size = 20;
+Box::$number = 20;
 
-$job = new Job();
-$job->task();
+//var_dump($box1, $box2);
+var_dump(Box::$number, Box::$number);
+Box::me1();
+Box::me2();
+MetalBox::me1();
+MetalBox::me2();
