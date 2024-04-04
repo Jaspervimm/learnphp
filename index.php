307cbaa643dfda3bc1b80ec88ae3f60bb2709bf7
Jasper Vimm <jasper.vimm@gmail.com>
2024-04-04 13:27:11 +0300

    Old website structure

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+$name = 'Jasper';
+$fruits = ['Apple', 'Banana', 'Cherry', 'Pear'];
+?>
+<?php include('partials/header.php'); ?>
+    <h1>Hello <?$name?></h1>
+    <ul>
+        <?php foreach ($fruits as $fruit) : ?>
+            <?php if ($fruits != 'Apple') : ?>
+                <li>
+                    <?=$fruit?>
+                </li>
+            <?php else : ?>
+                <li>
+                    <i>
+                        <?=$fruit?>
+                    </i>
+                </li>
+            <?php endif; ?>
+        <?php endforeach; ?>
+    </ul>
+<?php include('partials/footer.php'); ?>
